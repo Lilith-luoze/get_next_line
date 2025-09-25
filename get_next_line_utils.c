a434b05ebd9f8c7709479a7dfa32d201199affbf
luozguo <anitadou9@gmail.com>
2025-09-25 17:51:26 +0200

    log theorecitical code before finding an old function-strlen_char

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,29 +1,5 @@
 #include "get_next_line.h"
 
-/// @brief starting from the first char after \n, save the rest as new leftover.Append \0 at the end. Free the old leftover. 
-/// @param s
-/// @param nl pointer to the first \n in s 
-/// @return the new leftover with nul-term. NULL if no leftover or malloc fail.
-char	*save_leftover(char *s, char *nl)
-{
-	int		i;
-	char	*new_left;
-	
-	if (!s || !nl) // extra guard
-		return (NULL);
-	new_left = malloc(ft_strlen(nl + 1) + 1);
-	if (!new_left)
-		return (NULL);
-	i = 0;
-	while ((nl+1)[i] != '\0')
-	{
-		new_left[i] = (nl+1)[i];
-		i++;
-	}
-	new_left[i] = '\0';
-	free(s);
-	return (new_left);
-}
 
 /// @brief go through param s to find the first new line and return it.
 /// @param s 
@@ -76,9 +52,9 @@ char	*split_or_join_with_malloc(char **buf, char *pending_content, int bytes_rea
 	char	*new_content;
 	int	len_new_content;
 
-	len_new_content = bytes_read + len_pendingcnt;
 	if (!next_line && !bytes_read)
 	{
+		len_new_content = bytes_read + len_pendingcnt;
 		if (len_new_content)
 		{
 			new_content = malloc(len_new_content +1);
@@ -89,10 +65,10 @@ char	*split_or_join_with_malloc(char **buf, char *pending_content, int bytes_rea
 		}
 		return NULL;
 	}
-	split
-	line = generate_line_nl(*left , nl , len);
-	*left = save_leftover(*left , nl);
-	return (line);
+	else if(next_line)
+	{
+		
+	}
 }
 
 /// @brief count length of string ends with \0.
@@ -107,42 +83,3 @@ int	ft_strlen(char *s)
 		i++;
 	return (i);
 }
-
-
-
-/// @brief make the whole leftover as the newline. set static leftover to NULL. (essentially switch the string it points to)
-/// @param leftover 
-/// @return the whole leftover as the new line. NULL if leftover is NULL or malloc fail.
-char *leftover_as_new_line(char **leftover)
-{
-	char *line;
-
-	line = *leftover;
-	*leftover = NULL;
-	return (line);
-}
-
-/// @brief  duplicate the new line with \n. The end pointer to \n is given as parameter.
-/// @param s 
-/// @param nl 
-/// @return a malloced line with \n. NULL if s is NULL or empty string or malloc fail.
-char *generate_line_nl(char *s, char *nl , int len) 
-{
-	char	*dup;
-	int		i;
-
-	if (!s || !nl) // extra guard
-		return (NULL);
-	dup = (char *)malloc(len+1);
-	if (!dup)
-		return (NULL);
-	i = 0;
-	while ((s + i - 1) != nl)
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-

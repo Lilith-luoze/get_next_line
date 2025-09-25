598f8b4b129656cb9b20a6d48d173d965d55121f
luozguo <anitadou9@gmail.com>
2025-09-25 18:41:30 +0200

    2 hour non-stopping working is just enough per time

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -20,9 +20,7 @@ char	*where_is_newline(char *buf_terminated)
 	return (NULL);
 }
 
-/// @brief join two strs ending with '\0' into a malloced param. Before returning the new filled content, free the old one that is unused.
-/// @param left 
-/// @param buf 
+/// @brief join two strs ending with sep into a malloced param. Before returning the new filled content, free the old one that is unused.
 /// @return a new malloced str. NULL if malloc fail.
 char	*ft_join_strs(char *pending_content, char **buf , char *new_content)
 {
@@ -41,8 +39,42 @@ char	*ft_join_strs(char *pending_content, char **buf , char *new_content)
 	while ((*buf)[j])
 		new_content[i++] = (*buf)[j++];
 	new_content[i] = '\0';
+	(*buf)[0] = '\0';
 	free(pending_content);
 	return (new_content);
+}
+
+/// @brief split buf by the 1st sep. Update the buf with the leftover part. notice that user has to make sure at lease one sep exists or segmentfault.
+/// @param buf 
+/// @param sep 
+/// @return the first part array buf
+char *ft_split_strs(char **buf, char sep)
+{
+	char *to_append[BUFFER_SIZE + 1];
+	int i;
+	int j;
+
+	i = 0;
+	while ((*buf)[i] != sep)
+	{
+		to_append[i] = (*buf)[i];
+		i++;
+	}
+	to_append[i] = sep;
+	i++;
+	j = 0; 
+	
+	(*buf)[j] = (*buf)[i];
+	return to_append;
+}
+
+/// @brief copy one whole str to dst, then part of another str (until param sep) to dst, nul term it. copy left part to the original second str. Free the whole str.
+/// @param  
+/// @param  
+/// @param  
+split_buf_and_join_to_cnt(pending_content, buf, new_content, next_line)
+{
+
 }
 
 /// @brief two case (todo: create first) : ------b) \n ? split: return the 1st half part, and store the latter half to static buf c) no \n and EOF? (the condition can be refined if needed) : combined with b) return the whole thing.
@@ -67,19 +99,25 @@ char	*split_or_join_with_malloc(char **buf, char *pending_content, int bytes_rea
 	}
 	else if(next_line)
 	{
-		
+		len_new_content = len_pendingcnt + ft_strlen_sep(*buf , '\n') + 1;
+		new_content = malloc(len_new_content +1);
+		if (!new_content)
+				return NULL;
+		new_content = split_buf_and_join_to_cnt(pending_content, buf, new_content , next_line);
 	}
+	return new_content;
 }
 
-/// @brief count length of string ends with \0.
+/// @brief count length of string ends with param the 1st char of sep. (notice if sep isn't '\0', then at least one sep must exist in s , or else it will cause segfault)
 /// @param s 
-/// @return length of str. If NULL then 0.
-int	ft_strlen(char *s)
+/// @param sep
+/// @return length of str until first sep. If NULL then 0.
+int	ft_strlen_sep(char *s , char sep)
 {
 	int	i;
 
 	i = 0;
-	while (s && s[i] != '\0')
+	while (s && s[i] != sep)
 		i++;
 	return (i);
 }
